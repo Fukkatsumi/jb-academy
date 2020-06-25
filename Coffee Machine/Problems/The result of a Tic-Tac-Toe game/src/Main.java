9d979b10403751d606691b6ecb6271ee17c5dca4
Fukkatsumi <bogachenko.nas@gmail.com>
2020-06-25 14:15:42 +0300

    JB Academy

---
+++ Coffee Machine/Problems/The result of a Tic-Tac-Toe game/src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String x = "X";
+        String o = "O";
+        String space = " ";
+        String enter = "\n";
+        System.out.println(o + space + x + space + x + enter
+                + o + space + x + space + o + enter
+                + x + space + o + space + x + enter);
+    }
+}

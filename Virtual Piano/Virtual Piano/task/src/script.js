a144c7532cdf8047455a93bb61006b3618d264ff
Fukkatsumi <bogachenko.nas@gmail.com>
2020-09-15 01:18:17 +0300

    Virtual Piano progress 4/5

--- Virtual Piano/Virtual Piano/task/src/script.js
+++ Virtual Piano/Virtual Piano/task/src/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("keydown", function(event) {
     let key = event.key.toUpperCase();
+    let audio = new Audio("../keys/"+ key +".mp3");
 
     console.log(`The '${key}' key is pressed`);
+    audio.play();
 });
